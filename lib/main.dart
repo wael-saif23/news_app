ff07bb076aaa331a4fbdafa8073a2623ba74b9fe
wael-saif23 <ws.mywork@gmail.com>
2023-12-27 19:38:20 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      
+    );
+  }
+}
---
+++ lib/models/category_model.dart
@@ -0,0 +1,6 @@
+class CategoryModel {
+  final String imageAssetUrl;
+  final String categoryName;
+
+  const CategoryModel({required this.imageAssetUrl, required this.categoryName});
+}
