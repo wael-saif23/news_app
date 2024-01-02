b65e55a97251c1a3f96c9dea01c85e7cc7817362
wael-saif23 <ws.mywork@gmail.com>
2024-01-03 00:14:52 +0200

    rename new services

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app/services/news_servies.dart';
+import 'package:news_app/services/news_services.dart';
 import 'package:news_app/views/home_view.dart';
 
 void main() {
--- lib/widgets/news_list-builder.dart
+++ lib/widgets/news_list-builder.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/models/news_model.dart';
-import 'package:news_app/services/news_servies.dart';
+import 'package:news_app/services/news_services.dart';
 
 import 'package:news_app/widgets/news_list.dart';
 
