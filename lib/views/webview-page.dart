9f9b3e47a3ed8f9ec7e4ea558f78b01cb48d728a
wael-saif23 <ws.mywork@gmail.com>
2024-01-07 11:32:47 +0200

    add navigation control icons to pages

--- lib/views/webview-page.dart
+++ lib/views/webview-page.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/widgets/navigation_controls.dart.dart';
 import 'package:news_app/widgets/webview_stack.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewPage extends StatelessWidget {
+class WebViewPage extends StatefulWidget {
   const WebViewPage({super.key, required this.url});
   final String url;
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..loadRequest(
+        Uri.parse(widget.url),
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          actions: [
+            NavigationControls(controller: controller),
+          ],
+        ),
         body: WebViewStack(
-      pageUrl: url,
-    ));
+          controller: controller,
+        ));
   }
 }
--- lib/widgets/webview_stack.dart
+++ lib/widgets/webview_stack.dart
@@ -2,40 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewStack extends StatefulWidget {
-  const WebViewStack({super.key, required this.pageUrl});
-  final String pageUrl;
+  const WebViewStack({super.key, required this.controller});
+
+  final WebViewController controller;
   @override
   State<WebViewStack> createState() => _WebViewStackState();
 }
 
 class _WebViewStackState extends State<WebViewStack> {
   var loadingPercentage = 0;
-  late final WebViewController controller;
 
   @override
   void initState() {
     super.initState();
-    controller = WebViewController()
-      ..setNavigationDelegate(NavigationDelegate(
-        onPageStarted: (url) {
-          setState(() {
-            loadingPercentage = 0;
-          });
-        },
-        onProgress: (progress) {
-          setState(() {
-            loadingPercentage = progress;
-          });
-        },
-        onPageFinished: (url) {
-          setState(() {
-            loadingPercentage = 100;
-          });
-        },
-      ))
-      ..loadRequest(
-        Uri.parse(widget.pageUrl),
-      );
+    widget.controller.setNavigationDelegate(NavigationDelegate(
+      onPageStarted: (url) {
+        setState(() {
+          loadingPercentage = 0;
+        });
+      },
+      onProgress: (progress) {
+        setState(() {
+          loadingPercentage = progress;
+        });
+      },
+      onPageFinished: (url) {
+        setState(() {
+          loadingPercentage = 100;
+        });
+      },
+    ));
   }
 
   @override
@@ -43,7 +39,7 @@ class _WebViewStackState extends State<WebViewStack> {
     return Stack(
       children: [
         WebViewWidget(
-          controller: controller,
+          controller: widget.controller,
         ),
         if (loadingPercentage < 100)
           Padding(
